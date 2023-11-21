f082e7b622fb8f29d3ab43bfc7596b0a92e45584
Chris Edson <christopher.edson@gmail.com>
2023-11-21 12:25:09 +0000

    Install ImageProgress

---
+++ src/components/ImageProgress/ImageProgress.tsx
@@ -0,0 +1,8 @@
+import { CircleSnail } from "react-native-progress";
+
+/**
+ * Adds a loading bar for images as they are downloaded
+ */
+const ImageProgress = (): JSX.Element => <CircleSnail />;
+
+export default ImageProgress;
